b20cdfe394e9fcdd1bc4b1f865ce5b9256b88357
Vivek Verma <vivekverma6594@gmail.com>
2024-12-03 13:41:44 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 terraform {
   backend "s3" {
-    bucket         = "{your state bucket}"   
+    bucket         = "terraform-remote-state-vivek"   
     key            = "terraform.tfstate"
     region         = "us-west-2"
     encrypt        = true
